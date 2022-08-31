8a85a0cd7c63fa5f7a90e2faf355f7187cfc43a0
anilyaris <anilyaris@gmail.com>
2022-08-31 11:40:47 +0300

    Create newdata-restore.sh

---
+++ scripts/newdata-restore.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+host="localhost"
+data=""
+
+while getopts "h:d:" o
+do
+  case $o in
+  h)  host=$OPTARG ;;
+  d)  data=$OPTARG ;;
+  esac
+done
+
+psql -d postgres -h $host -c "COPY mongo_issue_comments FROM $data/issue_comments.csv WITH (FORMAT 'csv')"
+psql -d postgres -h $host -c "COPY mongo_commits FROM $data/commits.csv WITH (FORMAT 'csv')"
+psql -d postgres -h $host -c "COPY mongo_issues FROM $data/issues.csv WITH (FORMAT 'csv')"
+psql -d postgres -h $host -c "COPY mongo_pull_requests FROM $data/pull_requests.csv WITH (FORMAT 'csv')"
+psql -d postgres -h $host -c "COPY mongo_projects FROM $data/repos.csv WITH (FORMAT 'csv')"
+
+#: ft=bash
