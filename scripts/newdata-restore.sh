c1fedbe08b831ce17a7051484adfe09181e25891
anilyaris <anilyaris@gmail.com>
2022-08-31 12:16:34 +0300

    Update newdata-restore.sh

--- scripts/newdata-restore.sh
+++ scripts/newdata-restore.sh
@@ -11,13 +11,13 @@ do
   esac
 done
 
-psql -d postgres -h $host -c "COPY mongo_issue_comments FROM $data/issue_comments.csv WITH (FORMAT 'csv')"
-psql -d postgres -h $host -c "COPY mongo_commits FROM $data/commits.csv WITH (FORMAT 'csv')"
-psql -d postgres -h $host -c "COPY mongo_issues FROM $data/issues.csv WITH (FORMAT 'csv')"
-psql -d postgres -h $host -c "COPY mongo_pull_requests FROM $data/pull_requests.csv WITH (FORMAT 'csv')"
-psql -d postgres -h $host -c "COPY mongo_projects FROM $data/repos.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_issue_comments FROM $data/issue_comments.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_commits FROM $data/commits.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_issues FROM $data/issues.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_pull_requests FROM $data/pull_requests.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_projects FROM $data/repos.csv WITH (FORMAT 'csv')"
 
-psql -d postgres -h $host -c "COPY mongo_issue_comments FROM $data/new_comments.csv WITH (FORMAT 'csv')"
-psql -d postgres -h $host -c "COPY mongo_issues FROM $data/new_issues.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_issue_comments FROM $data/new_comments.csv WITH (FORMAT 'csv')"
+psql -d ghtorrent -U ghtorrent -h $host -c "COPY mongo_issues FROM $data/new_issues.csv WITH (FORMAT 'csv')"
 
 #: ft=bash
